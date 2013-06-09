2f54b07a27cc671b8d40121ddfa95089a5fe0a81
Fredrik Wendt <fredrik@wendt.se>
2013-06-09 10:08:18 +0200

    cleaned up exercise

--- exercise/src/main/java/solution/NameTagManagerImplementation.java
+++ exercise/src/main/java/solution/NameTagManagerImplementation.java
@@ -1,43 +1,23 @@
 package solution;
 
-import java.awt.Image;
-
 import com.squeed.kata.nametag.NameTagManager;
-import com.squeed.kata.nametag.entities.Attendee;
 import com.squeed.kata.nametag.entities.Event;
-import com.squeed.kata.nametag.entities.Photo;
-import com.squeed.kata.nametag.services.PhotoDownloadResult;
-import com.squeed.kata.nametag.services.PhotoDownloadService;
 import com.squeed.kata.nametag.services.PhotoDownloadServiceManager;
 import com.squeed.kata.nametag.services.PrintingService;
 
 public class NameTagManagerImplementation implements NameTagManager {
-	
-	private final PhotoDownloadServiceManager photoDownloadManager;
+
+	private final PhotoDownloadServiceManager downloadManager;
 	private final PrintingService printingService;
 
-	public NameTagManagerImplementation(PhotoDownloadServiceManager photoDownloadManager, PrintingService nameTagPrinter) {
-		this.photoDownloadManager = photoDownloadManager;
-		this.printingService = nameTagPrinter;
+	public NameTagManagerImplementation(PhotoDownloadServiceManager downloadManager, PrintingService printingService) {
+		this.downloadManager = downloadManager;
+		this.printingService = printingService;
 	}
 
 	@Override
 	public void printNameTagsForEvent(Event event) {
-		for (Attendee visitor : event.getExpectedAttendees()) {
-			Image imageToPrint = getImageToPrintFor(visitor, event);
-			printingService.printNameTagFor(visitor, imageToPrint);
-		}
-	}
-
-	private Image getImageToPrintFor(Attendee visitor, Event event) {
-		for (PhotoDownloadService downloader : photoDownloadManager.getDownloadServices()) {
-			PhotoDownloadResult downloadResult = downloader.downloadPhotoOf(visitor);
-			if (downloadResult.photoWasDownloaded()) {
-				Photo downloadedImage = downloadResult.get();
-				return downloadedImage.getImageData();
-			}
-		}
-		return event.getDefaultImage();
+		// FIXME: implement this
 	}
 
 }
